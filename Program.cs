2e64e7ac2493f53d01088288ca6044e5b1e631fa
niko5443 <184934855+Nika221-bit@users.noreply.github.com>
2026-05-24 19:25:27 +0400

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//თავიდან ამის გამოგზავნას არ ვაპირებდი,მაგიტომ აქვს დიდი სახელები :D
+
+Console.WriteLine("hello world");
+int ricxvi = 22;
+long didiricxvi = 900000000000000000;
+float iracionaluriRicxvi = 2.22f;
+double iracionaluriRicxviDouble = 2.2222222222;
+decimal iracionaluriRicxviDecimal = 2.2222222222222222222222222222m;
+string text = "gamarjoba";
+char character = 'N';
+bool isTrue = true;
+
+Console.WriteLine(ricxvi);
+Console.WriteLine(didiricxvi);
+Console.WriteLine(iracionaluriRicxvi);
+Console.WriteLine(iracionaluriRicxviDouble);
+Console.WriteLine(iracionaluriRicxviDecimal);
+Console.WriteLine(text);
+Console.WriteLine(character);
+Console.WriteLine(isTrue);
+Console.WriteLine(text + " " + character);
+if (isTrue)
+{
+    Console.WriteLine(character);
+}
